9d5aa932c60d1e1e95b37e08794eae6779b14114
Stephen Krochenski <skrochen@krotek.com>
2023-10-07 23:17:52 -0400

    Day 1 part 1 and 2 solutions, gitignore

--- aoc_2022-1b.cbl
+++ aoc_2022-1b.cbl
@@ -1,8 +1,8 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. aoc_2022-1a.
+       PROGRAM-ID. aoc_2022-1b.
        AUTHOR. SK.
-       DATE-WRITTEN. Sep 17 2023.
-       REMARKS. Advent of Code 2022 Day 1 Part 1.
+       DATE-WRITTEN. Oct 7 2023.
+       REMARKS. Advent of Code 2022 Day 1 Part 2.
 
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
@@ -19,8 +19,9 @@
        WORKING-STORAGE SECTION.
        01 WS-EOF                  PIC A(1)  VALUE 'F'.
        01 WS-CALORIES-AS-NUM      PIC 9(10) VALUE 0.
-       01 WS-TOTAL-CALORIES       PIC 9(12) VALUE 0.
-       01 WS-MAX-CALORIES         PIC 9(12) VALUE 0.
+       01 WS-MAX-CALORIES-TABLE.
+       05 WS-MAX-CALORIES         PIC 9(12) VALUE 0 OCCURS 4 TIMES.
+       01 WS-SUM-3-CALORIES       PIC 9(12).
        01 WS-MAX-CALORIES-TO-DISP PIC Z(11)9(1). *> disp at least 1 zero  
 
        PROCEDURE DIVISION.
@@ -32,18 +33,18 @@
  	     NOT AT END
 	       IF CALORIES <> ' '
 	         MOVE CALORIES TO WS-CALORIES-AS-NUM
-	         ADD WS-CALORIES-AS-NUM TO WS-TOTAL-CALORIES
+	         ADD WS-CALORIES-AS-NUM TO WS-MAX-CALORIES(4) *> use as a buffer
       *	         END-ADD
-               ELSE *> end of this elf's calories
-   	         IF WS-TOTAL-CALORIES > WS-MAX-CALORIES
-  		   MOVE WS-TOTAL-CALORIES TO WS-MAX-CALORIES
-    		 END-IF
-	         MOVE 0 TO WS-TOTAL-CALORIES *> Reset
+	       ELSE *> end of this elf's calorie count
+    		 SORT WS-MAX-CALORIES ON DESCENDING KEY WS-MAX-CALORIES
+	         MOVE 0 TO WS-MAX-CALORIES(4) *> Reset
 	       END-IF
  	     END-READ
 	   END-PERFORM
            CLOSE ELVES
-      *    Format MAX_CALORIES to suppress leading zeros
-   	   MOVE WS-MAX-CALORIES TO WS-MAX-CALORIES-TO-DISP
-  	   DISPLAY  "Max Calories: " WS-MAX-CALORIES-TO-DISP
+   	   COMPUTE WS-SUM-3-CALORIES = FUNCTION SUM(WS-MAX-CALORIES(1),
+  	     WS-MAX-CALORIES(2), WS-MAX-CALORIES(3))
+   	   MOVE WS-SUM-3-CALORIES TO WS-MAX-CALORIES-TO-DISP
+  	   DISPLAY  "Total calories of top 3 elves: "
+  	     WS-MAX-CALORIES-TO-DISP
            STOP RUN.
